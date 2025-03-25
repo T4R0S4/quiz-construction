8074bb9f15c73f73f69ac4dd030cfdbe2096f9d7
T4R0S4 <tjhai.minkong12@gmail.com>
2025-03-25 18:24:46 +0700

    add semua file yang diperlukan

---
+++ quiz-construction/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AplikasiKuisKonstruksi from './components/hanyu'; // Pastikan nama dan path-nya sesuai
+
+function App() {
+  return (
+    <div>
+      <AplikasiKuisKonstruksi />
+    </div>
+  );
+}
+
+export default App;
